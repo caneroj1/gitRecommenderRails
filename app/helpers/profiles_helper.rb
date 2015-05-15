1bb61591d7d56c315bb654bcfdba2e08ddde31f0
Joe Canero <caneroj1@tcnj.edu>
2015-05-15 16:32:54 -0400

    added modal for recommended repositories.

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -12,4 +12,12 @@ module ProfilesHelper
   def repository_description(repo)
     repo.description || "No description given"
   end
+
+  def favorability_string(score)
+    red = score.eql?(200) ? 220 : score
+    green = score.eql?(200) ? 30 : 255 - score
+    width = (200 - score)/200.0 * 100
+
+    "background-color: rgb(#{red}, #{green}, 0); width: #{width}%; height: 25px;"
+  end
 end
